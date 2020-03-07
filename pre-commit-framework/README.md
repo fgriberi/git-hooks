d1a2feed21702802ec703a82d3a4288ab907a339
Franco Riberi <fgriberi@gmail.com>
2020-03-07 13:32:22 -0300

    [test-pre-commit-framework] upload configuration without install it

---
+++ pre-commit-framework/README.md
@@ -0,0 +1,6 @@
+# Testing pre-commit hook
+
+```bash
+$ pip install -r requirements.txt
+$ pre-commit install
+```
---
+++ pre-commit-framework/test-src/test_two.py
@@ -0,0 +1,22 @@
+import os
+import sys
+
+class MyClass(object):
+
+  def set_val(self, val):
+        self.value = val
+
+    def get_val(self):
+        print(self.value)
+        print('******************** This is a long print line ********************')
+        print('************************** This is a another long print line **********************************************')
+        return self.value
+
+a = MyClass()
+b = MyClass()
+
+a.set_val(10)
+b.set_val(100)
+
+a.get_val()
+b.get_val()
